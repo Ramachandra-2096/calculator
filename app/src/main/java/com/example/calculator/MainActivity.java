f85a6ab36679d22fa3e5f239c27ae11299e8ef68
Ramachandra-2096 <ramachandraudupa2004@gmail.com>
2023-10-28 13:20:47 +0530

    second commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
+<<<<<<< HEAD
 
     public class MainActivity extends AppCompatActivity {
         Button button, btnac, btnc;
@@ -52,6 +53,54 @@ import java.text.DecimalFormat;
             {
                 txt.setText(txt.getText().toString()+"^2");
             }else {
+=======
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+    Button button, btnac, btnc;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.textView);
+        btnac = findViewById(R.id.bac);
+        btnc = findViewById(R.id.bc);
+
+        btnac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("");
+            }
+        });
+
+        btnc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentText = txt.getText().toString();
+                if (!currentText.isEmpty()) {
+                    txt.setText(currentText.substring(0, currentText.length() - 1));
+                }
+            }
+        });
+    }
+
+    public void num(View view) {
+        Button button = (Button) view;
+        String currentText = txt.getText().toString();
+        txt.setText(currentText + button.getText().toString());
+    }
+
+    public void op(View view) {
+        button = (Button) view;
+        String currentText = txt.getText().toString();
+        if (button.getText().toString().equals("Χ²")) {
+            txt.setText(txt.getText().toString() + "^2");
+        } else {
+>>>>>>> 92f571b (second commit)
             if (!button.getText().toString().equals("=")) {
                 if (currentText.isEmpty() || isOperator(currentText.charAt(currentText.length() - 1))) {
 
@@ -67,6 +116,7 @@ import java.text.DecimalFormat;
                     txt.setText("Error");
                 }
             }
+<<<<<<< HEAD
             }
         }
 
@@ -131,3 +181,111 @@ import java.text.DecimalFormat;
             return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '^';
         }
     }
+=======
+        }
+    }
+
+    private static double evaluateExpression(String expression) {
+        expression = expression.replaceAll(" ", "").trim();
+        try {
+            return evaluateExpressionHelper(expression);
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            throw new ArithmeticException("Invalid expression");
+        }
+    }
+
+    private static double evaluateExpressionHelper(String expression) {
+        Queue<String> postfix = infixToPostfix(expression);
+        return evaluatePostfix(postfix);
+    }
+
+    private static Queue<String> infixToPostfix(String expression) {
+        Queue<String> output = new LinkedList<>();
+        Stack<Character> operators = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char token = expression.charAt(i);
+
+            if (Character.isDigit(token)) {
+                StringBuilder operand = new StringBuilder();
+                while (i < expression.length() && (Character.isDigit(expression.charAt(i)) || expression.charAt(i) == '.')) {
+                    operand.append(expression.charAt(i));
+                    i++;
+                }
+                output.add(operand.toString());
+                i--; // Move back one step as the outer loop will also increment 'i'
+            } else if (isOperator(token)) {
+                while (!operators.isEmpty() && getPrecedence(operators.peek()) >= getPrecedence(token)) {
+                    output.add(operators.pop().toString());
+                }
+                operators.push(token);
+            }
+        }
+
+        while (!operators.isEmpty()) {
+            output.add(operators.pop().toString());
+        }
+
+        return output;
+    }
+
+    private static double evaluatePostfix(Queue<String> postfix) {
+        Stack<Double> operands = new Stack<>();
+
+        for (String token : postfix) {
+            if (isNumeric(token)) {
+                operands.push(Double.parseDouble(token));
+            } else {
+                double b = operands.pop();
+                double a = operands.pop();
+                double result = applyOperator(a, b, token);
+                operands.push(result);
+            }
+        }
+
+        return operands.pop();
+    }
+
+    private static int getPrecedence(char operator) {
+        if (operator == '^') {
+            return 3;
+        } else if (operator == 'X' || operator == '/') {
+            return 2;
+        } else if (operator == '+' || operator == '-') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static boolean isNumeric(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    private static double applyOperator(double a, double b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "X":
+                return a * b;
+            case "/":
+                if (b == 0) {
+                    throw new ArithmeticException("Division by zero");
+                }
+                return a / b;
+            case "^":
+                return Math.pow(a, b);
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == 'X' || c == '/' || c == '^';
+    }
+
+}
+>>>>>>> 92f571b (second commit)
